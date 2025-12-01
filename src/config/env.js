b8125d6d2a28c7d954f5f279b4941dcb339dcf9b
EUNGWANCHOI <silvergwan@gmail.com>
2025-12-01 09:04:16 +0900

    update: add env content about supabase(수파베이스 관련 env 내용 추가)

--- src/config/env.js
+++ src/config/env.js
@@ -3,5 +3,7 @@ dotenv.config();
 
 export const config = {
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_KEY: process.env.SUPABASE_KEY,
   PORT: process.env.PORT || 3000,
 };
